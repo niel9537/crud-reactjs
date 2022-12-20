94b002dea43bfc0a054c73a0f1cce3c29d9e0a77
niel9537 <niel.zebua8@gmail.com>
2022-12-20 11:45:07 +0700

    add:new project

---
+++ crud-reactjs/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Create from "./components/create";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="main">
+      <div>
+        <Create />
+      </div>
+    </div>
+  );
+}
+
+export default App;
